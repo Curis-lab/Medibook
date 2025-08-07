289ad6934724ac4d18db5ce8f9d5cb1d8e75fe7d
Boris <72118807+Curis-lab@users.noreply.github.com>
2025-08-08 03:56:00 +0630

    change config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://medibook-g32h.onrender.com/api/v1';
-export const BASE_URL= 'http://localhost:8000/api/v1';
+export const BASE_URL = 'https://medibook-g32h.onrender.com/api/v1';
+// export const BASE_URL= 'http://localhost:8000/api/v1';
 export const token = localStorage.getItem('token');
 export const user = JSON.parse(localStorage.getItem('user'));
